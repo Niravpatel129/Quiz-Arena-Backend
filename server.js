3de18c89de84fb3111c2e6dc4a4fea7b769e35b6
Nirav <niravpatelp129@gmail.com>
2023-12-21 07:46:52 -0500

    * chore(server.js): update cors configuration to allow requests from 'http://localhost:19006' with credentials

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, {});
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:19006',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(routes);
 
