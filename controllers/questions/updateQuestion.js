8874d98b4f300879cc3061b9ce725fa0b9ffc57d
Nirav <niravpatelp129@gmail.com>
2023-12-23 04:44:03 -0500

    * fix(updateQuestion.js): fix updating question logic and error handling

--- controllers/questions/updateQuestion.js
+++ controllers/questions/updateQuestion.js
@@ -4,15 +4,21 @@ const updateQuestion = async (req, res) => {
   try {
     const { id } = req.params;
     const questionToUpdate = await Question.findById(id);
-    if (questionToUpdate) {
-      questionToUpdate = req.body;
-      await questionToUpdate.save();
-      res.json(questionToUpdate);
-    } else {
-      res.status(404).json({ error: 'Question not found' });
+    if (!questionToUpdate) {
+      return res.status(404).json({ message: 'Question not found' });
     }
+
+    // make the req.body new
+    const newQuestion = req.body;
+
+    questionToUpdate.set(newQuestion);
+
+    await questionToUpdate.save();
+
+    return res.status(200).json(questionToUpdate);
   } catch (err) {
     console.log(err);
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
 
