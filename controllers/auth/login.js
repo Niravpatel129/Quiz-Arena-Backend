dc90cfda6efc84eaef47df18762c3d38ae3b38bc
Nirav <niravpatelp129@gmail.com>
2023-12-24 23:06:32 -0500

    Add register function and modify login function

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -3,6 +3,27 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 
+// Register function
+const register = async (username, password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return newUser;
+  } catch (err) {
+    console.error('register error: ', err.message);
+    throw err;
+  }
+};
+
+// Modified login function
 const login = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -15,12 +36,12 @@ const login = async (req, res) => {
     let user = await User.findOne({ username: new RegExp('^' + username + '$', 'i') });
 
     if (!user) {
-      return res.status(400).json({ msg: 'Invalid Credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: 'Invalid password' });
+      user = await register(username, password); // Register the user if not found
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid password' });
+      }
     }
 
     const payload = { user: { id: user._id, name: user.username } };
