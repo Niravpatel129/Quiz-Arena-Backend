b2d1f796f0b3087e44e24caf89315324c3cec6c1
Nirav <niravpatelp129@gmail.com>
2023-12-26 21:27:54 -0500

    Refactor checkConnection function to use async/await and retrieve session by ID

--- sockets/eventHandlers/game/checkConnection.js
+++ sockets/eventHandlers/game/checkConnection.js
@@ -1,10 +1,13 @@
+const GameSession = require('../../../models/GameSession');
 const gameSessionManager = require('../../utils/gameSessionManager');
 
 const checkConnection = (socket, io) => {
-  socket.on('check_connection', (gameData) => {
+  socket.on('check_connection', async (gameData) => {
     try {
-      console.log('🚀  gameData:', gameData);
-      if (gameData.gameSessionManager?.endTime) {
+      const session = await GameSession.findById(gameData.gameSession.sessionId);
+      console.log('🚀  session:', session);
+
+      if (session?.endTime) {
         socket.emit('connection_lost', { message: 'Connection successful' });
       }
     } catch (error) {
