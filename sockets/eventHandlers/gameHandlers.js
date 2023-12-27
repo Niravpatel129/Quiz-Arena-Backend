ccdc679545252724e9e34a36ecfb3f59ca285917
Nirav <niravpatelp129@gmail.com>
2023-12-26 21:17:56 -0500

    Add checkConnection event handler

--- sockets/eventHandlers/gameHandlers.js
+++ sockets/eventHandlers/gameHandlers.js
@@ -4,10 +4,12 @@ const joinGame = require('./game/joinGame');
 const startGame = require('./game/startGame');
 const submitAnswer = require('./game/submitAnswer');
 const leaveGame = require('./game/leaveGame');
+const checkConnection = require('./game/checkConnection');
 
 module.exports = (socket, io) => {
   joinGame(socket, io);
   startGame(socket, io);
   submitAnswer(socket, io);
   leaveGame(socket, io);
+  checkConnection(socket, io);
 };
