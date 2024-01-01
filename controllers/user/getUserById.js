92fa34bdd41b8258bec4bcfe5084e8cd30f6178d
Nirav <niravpatelp129@gmail.com>
2024-01-01 16:08:51 -0500

    Update getUserById controller to include additional user data

--- controllers/user/getUserById.js
+++ controllers/user/getUserById.js
@@ -3,7 +3,21 @@ const User = require('../../models/User');
 const getUserById = async (req, res) => {
   const { userId } = req.params;
   const user = await User.findById(userId);
-  res.send(user);
+  console.log('🚀  user:', user);
+
+  const DataToSend = {
+    userId: '1',
+    username: 'John Doe',
+    avatar:
+      'https://t4.ftcdn.net/jpg/05/69/84/67/360_F_569846700_i3o9u2fhPVVq7iJAzkqMqCwjWSyv53tT.jpg',
+    country: 'aq',
+    lastActive: '8 min ago',
+    averageRating: '2212',
+    totalGames: '11230',
+    winRate: '44%',
+  };
+
+  res.send(DataToSend);
 };
 
 module.exports = getUserById;
