2c4d72ef58f5200b48d2439ef20521cecf70cec4
Nirav <niravpatelp129@gmail.com>
2024-01-02 23:49:38 -0500

    Reorder routes and add getUserById route

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const userController = require('../controllers/user');
 const { verifyToken } = require('../sockets/middleware/jwtMiddleware');
 
 router.get('/', userController.getAllUsers);
-router.get('/:userId', verifyToken, userController.getUserById);
 router.get('/friends', verifyToken, userController.getFriends);
 router.post('/', userController.createUser);
-router.put('/:userId', verifyToken, userController.updateUser);
 router.post('/addFriend', verifyToken, userController.addFriend);
 router.get('/notifications', verifyToken, userController.getUserNotifications);
 router.post('/notifications', verifyToken, userController.createUserNotification);
 router.delete('/notifications/:notificationId', verifyToken, userController.deleteUserNotification);
+router.put('/:userId', verifyToken, userController.updateUser);
+router.get('/:userId', verifyToken, userController.getUserById);
 
 module.exports = router;
