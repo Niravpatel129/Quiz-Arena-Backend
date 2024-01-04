b76ab501a965173a6e1565185d86ca8aa29c18dd
Nirav <niravpatelp129@gmail.com>
2024-01-04 12:17:26 -0500

    Refactor jwtMiddleware.js to handle error cases

--- sockets/middleware/jwtMiddleware.js
+++ sockets/middleware/jwtMiddleware.js
@@ -2,25 +2,30 @@ const jwt = require('jsonwebtoken');
 const bannedUsers = ['nvsroc1@gmail.com'];
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers?.cookie?.split('=')[1];
+  try {
+    const token = req.headers?.cookie?.split('=')[1];
 
-  if (!token) return next(401, 'You are not authenticated!');
+    if (!token) return next(401, 'You are not authenticated!');
 
-  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
-    if (err) {
-      console.log(err);
-      return next(403, 'Token is not valid!');
-    }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        console.log(err);
+        return next(403, 'Token is not valid!');
+      }
 
-    req.userId = payload.user.id;
+      req.userId = payload.user.id;
 
-    // dont allow any banned users
-    if (bannedUsers.includes(payload.user.username)) {
-      return next(createError(401, 'User not allowed!'));
-    }
+      // dont allow any banned users
+      if (bannedUsers.includes(payload.user.username)) {
+        return next(createError(401, 'User not allowed!'));
+      }
 
-    next();
-  });
+      next();
+    });
+  } catch (err) {
+    console.log(err);
+    next(500, 'Something went wrong!');
+  }
 };
 
 module.exports = {
