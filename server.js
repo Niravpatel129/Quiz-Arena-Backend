754d4b1545a4126fb56b6b258679745a603285f7
Nirav <niravpatelp129@gmail.com>
2024-01-04 09:38:17 -0500

    Add Sentry for error monitoring and performance profiling

--- server.js
+++ server.js
@@ -6,11 +6,32 @@ const routes = require('./routes');
 const initializeSockets = require('./sockets');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-
+const Sentry = require('@sentry/node');
+const { ProfilingIntegration } = require('@sentry/profiling-node');
 const app = express();
 const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING, {});
+
+Sentry.init({
+  dsn: 'https://c0dbf20d149da4a761f7e206f24b7554@o1363835.ingest.sentry.io/4506513494048768',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(
   cors({
@@ -25,9 +46,12 @@ app.use(
     credentials: true,
   }),
 );
-app.use(express.json());
-app.use(routes);
 app.use(cookieParser());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => {
