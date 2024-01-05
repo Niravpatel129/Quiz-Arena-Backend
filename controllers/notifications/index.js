571aa737601c3383400f903aabe4a111792e310f
Nirav <niravpatelp129@gmail.com>
2024-01-04 20:20:00 -0500

    Remove commented out code and add notifications route

--- controllers/notifications/index.js
+++ controllers/notifications/index.js
@@ -2,6 +2,4 @@ const register = require('./register');
 
 module.exports = {
   register,
-  //   deleteQuestion: require('./deleteQuestion'),
-  //   updateQuestion: require('./updateQuestion'),
 };
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.use('/leaderboards', require('./leaderboards'));
 router.use('/matchHistory', require('./matchHistory'));
 router.use('/chat', require('./chat'));
 router.use('/homepage', require('./homepage'));
+router.use('/notifications', require('./notifications'));
 
 // Exporting the base router
 module.exports = router;
