a38de10b8fb5730a821ae35c02e14983e61a0783
Nirav <niravpatelp129@gmail.com>
2024-01-05 16:08:09 -0500

    Add deleteUser functionality and route

--- controllers/user/deleteUser.js
+++ controllers/user/deleteUser.js
@@ -1,3 +1,16 @@
-const deleteUser = async (req, res) => {};
+const User = require('../../models/User');
+
+const deleteUser = async (req, res) => {
+  const userId = req.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    console.log('🚀  user deleted');
+
+    res.send({ message: 'User deleted successfully' });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = deleteUser;
