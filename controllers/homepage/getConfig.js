26217f881aca040c8b7e8b0a7c82ebb2043e87ca
Nirav <niravpatelp129@gmail.com>
2024-01-08 14:05:59 -0500

    Add getConfig controller for homepage

---
+++ controllers/homepage/getConfig.js
@@ -0,0 +1,14 @@
+const getConfig = async (req, res) => {
+  try {
+    const config = {
+      queueTime: 30,
+    };
+
+    res.status(200).json(config);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+module.exports = getConfig;
