811e62380f21018af41a0dee9dcfec51980ae4b1
Nirav <niravpatelp129@gmail.com>
2024-01-08 14:06:07 -0500

    Add getConfig to homepage controller and route

--- controllers/homepage/index.js
+++ controllers/homepage/index.js
@@ -1,7 +1,9 @@
 const getQuestionList = require('./getQuestionList');
+const getConfig = require('./getConfig.js');
 
 module.exports = {
   getQuestionList,
+  getConfig,
   //   deleteQuestion: require('./deleteQuestion'),
   //   updateQuestion: require('./updateQuestion'),
 };
--- routes/homepage.js
+++ routes/homepage.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homepageController = require('../controllers/homepage');
 
 router.get('/list', homepageController.getQuestionList);
+router.get('config', homepageController.getConfig);
 
 module.exports = router;
