d5c65c92b2a68765fb4a1164eee457cf4117a613
Nirav <niravpatelp129@gmail.com>
2024-01-09 14:04:03 -0500

    Refactor getRankingsForCategory controller to include avatar and country in the leaderboard

--- controllers/leaderboards/getRankingsForCategory.js
+++ controllers/leaderboards/getRankingsForCategory.js
@@ -6,14 +6,17 @@ const getLeaderboardByCategory = async (req, res) => {
 
     const users = await User.find({})
       .sort({ [`elo.rating.${category}`]: -1 })
-      .select(`_id username elo.rating.${category}`)
+      .select(`_id username profile.avatar profile.country elo.rating.${category}`)
       .limit(10);
+    console.log('🚀  users:', users);
 
     // Transform the query result to the desired format
     const leaderboard = users.map((user) => ({
       userId: user._id,
       username: user.username,
       rating: user.elo.rating[category],
+      avatar: user.profile.avatar,
+      country: user.profile.country,
     }));
 
     res.json(leaderboard);
