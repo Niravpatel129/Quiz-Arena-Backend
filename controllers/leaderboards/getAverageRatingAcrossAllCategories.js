ea569de151a3ed5adf7ca636fcbe8d010b823a8f
Nirav <niravpatelp129@gmail.com>
2024-01-10 13:08:16 -0500

    Add categoriesList helper and update getAverageRatingAcrossAllCategories controller

--- controllers/leaderboards/getAverageRatingAcrossAllCategories.js
+++ controllers/leaderboards/getAverageRatingAcrossAllCategories.js
@@ -1,14 +1,21 @@
+const categories = require('../../helpers/categoriesList');
 const User = require('../../models/User');
 
 const getAverageRatingAcrossAllCategories = async (req, res) => {
   try {
+    const allCategories = categories.map((category) => {
+      return category.subCategories.map((subCategory) => {
+        return subCategory.name;
+      });
+    });
+
     const averageRatings = await User.aggregate([
       {
         $project: {
           username: 1,
           profile: 1,
           averageRating: {
-            $avg: ['$elo.rating.logos', '$elo.rating.league of legends', '$elo.rating.valorant'],
+            $avg: allCategories.flat(1).map((category) => `$elo.rating.${category.toLowerCase()}`),
           },
         },
       },
