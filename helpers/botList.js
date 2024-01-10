10965fac9ecc99fd6c55ffd577265dc7be50a58d
Nirav <niravpatelp129@gmail.com>
2024-01-09 23:26:46 -0500

    Add new bots to botList

--- helpers/botList.js
+++ helpers/botList.js
@@ -4,6 +4,51 @@ const botList = [
     userId: '65986b8569ca51f17288d5e1',
     name: 'bunyy',
   },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTZezima',
+    userId: '659e190556ddf7999c4a35ed',
+    name: 'Zezima',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTChrish',
+    userId: '659e18cf56ddf7999c4a35e4',
+    name: 'Chrish',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTEyeslol',
+    userId: '659e194256ddf7999c4a35f6',
+    name: 'Eyeslol',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOT400iq',
+    userId: '659e197656ddf7999c4a360e',
+    name: '400iq',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTHarry',
+    userId: '659e19a256ddf7999c4a3616',
+    name: 'Harry',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTQuizzy',
+    userId: '659e19b956ddf7999c4a361e',
+    name: 'Quizzy',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTSgt.Lieut',
+    userId: '659e1a1156ddf7999c4a3627',
+    name: 'Sgt.Lieut',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTLadzone',
+    userId: '659e1b50bc06fc1bfa019962',
+    name: 'Ladzone',
+  },
+  {
+    socketId: 'EWw4E8ELTbxHZx7ZAAABOTSophiaaaa',
+    userId: '659e1b1fbc06fc1bfa01995a',
+    name: 'Sophiaaaa',
+  },
 ];
 
 module.exports = botList;
