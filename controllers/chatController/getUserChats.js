3f67d16751500685a5b553f0275d726c40306d68
Nirav <niravpatelp129@gmail.com>
2024-01-13 03:25:41 -0500

    Add userId validation and log chats

--- controllers/chatController/getUserChats.js
+++ controllers/chatController/getUserChats.js
@@ -3,9 +3,14 @@ const Chat = require('../../models/Chat');
 const getUserChats = async (req, res) => {
   try {
     const userId = req.userId;
+    if (!userId) return res.status(400).send('userId is required');
+
     const chats = await Chat.find({ participants: userId }).populate(
       'participants messages.sender',
     );
+
+    console.log('🚀  chats:', chats);
+
     res.json(chats);
   } catch (error) {
     res.status(500).send(error);
