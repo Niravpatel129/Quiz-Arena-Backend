29b7319c95a60d4c18ea74ca8d893c561d6601cc
Nirav <niravpatelp129@gmail.com>
2024-01-15 07:04:46 -0500

    Commented out version check in getConfig.js

--- controllers/homepage/getConfig.js
+++ controllers/homepage/getConfig.js
@@ -1,11 +1,11 @@
 const getConfig = async (req, res) => {
   try {
     const version = req?.params?.version;
-    let queueTime = Math.floor(Math.random() * 35) + 15;
+    let queueTime = Math.floor(Math.random() * 30) + 15;
 
-    if (version) {
-      queueTime = version === '5' ? 99999 : Math.floor(Math.random() * 35) + 15;
-    }
+    // if (version) {
+    //   queueTime = version === '5' ? 99999 : Math.floor(Math.random() * 35) + 15;
+    // }
 
     const config = {
       queueTime: queueTime,
