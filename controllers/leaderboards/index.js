1913113f419d0edb48be6fee0252eeaa2587e00c
Nirav <niravpatelp129@gmail.com>
2024-01-28 15:09:24 -0500

    Remove getLeaderboards from leaderboards controller

--- controllers/leaderboards/index.js
+++ controllers/leaderboards/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   getRankingsForCategory: require('./getRankingsForCategory'),
   getAverageRatingAcrossAllCategories: require('./getAverageRatingAcrossAllCategories'),
-  getLeaderboards: require('./getLeaderboards'),
   getLeaderboardsGlobal: require('./getLeaderboardsGlobal'),
   getLeaderboardsFriends: require('./getLeaderboardsFriends'),
 };
