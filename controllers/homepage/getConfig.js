10dd4f3a017a9b0d69415e0fa69b4abee8a89fc7
Nirav <niravpatelp129@gmail.com>
2024-02-04 14:13:21 -0500

    Fix bug in login functionality
    Update error handling in API calls
    
    Refactor code for better performance
    
    Add new feature for user authentication
    
    Update UI design for better user experience

--- controllers/homepage/getConfig.js
+++ controllers/homepage/getConfig.js
@@ -7,7 +7,7 @@ const getConfig = async (req, res) => {
     let queueTime = defaultQueueTime;
 
     if (version) {
-      queueTime = version === '40' ? 9999 : defaultQueueTime;
+      queueTime = version === '41' ? 9999 : defaultQueueTime;
     }
 
     if (parseInt(version) < 24) {
