e53ad25ed86842cf8f77a04bcef96c2d72645da6
Nirav <niravpatelp129@gmail.com>
2024-02-07 04:17:10 -0500

    Add Discord bot integration for sending messages to bots

--- controllers/chatController/sendMessage.js
+++ controllers/chatController/sendMessage.js
@@ -1,8 +1,11 @@
 const { sendPushNotifications } = require('../../helpers/sendPushNotifications');
 const Chat = require('../../models/Chat');
+const { sendMessageToChannel } = require('../../services/discord_bot');
+const botList = require('../../helpers/botList');
 
 const sendMessage = async (req, res) => {
   try {
+    console.log('🚀 ~ file: sendMessage.js ~ line 8 ~ sendMessage ~ req');
     const userId = req.userId;
     const chatId = req.params.chatId;
     const messageContent = req.body.message;
@@ -19,7 +22,28 @@ const sendMessage = async (req, res) => {
       path: 'participants',
       select: 'misc.pushToken username _id',
     });
+
+    const userInChat = chat.participants.find((user) => user._id.toString() === userId.toString());
     const otherUserId = chat.participants.find((user) => user._id.toString() !== userId.toString());
+    console.log('🚀  otherUserId:', otherUserId);
+
+    // if the otherUserId includes 'BOT' then send a message to the discord channel with the message
+
+    if (
+      botList.some((bot) => {
+        console.log('🚀  bot:', bot);
+        console.log('🚀  otherUserId.username:', otherUserId.username);
+        if (bot.name === otherUserId.username) return true;
+        // otherUserId.username.includes(bot);
+      })
+    ) {
+      console.log('opponent is a bot, sending message to discord channel');
+      sendMessageToChannel(
+        '1204714714580652092',
+        `**${userInChat.username}** send a message to **${otherUserId.username}(BOT)**: 
+${messageContent}`,
+      );
+    }
 
     if (otherUserId && otherUserId?.misc?.pushToken) {
       sendPushNotifications(
