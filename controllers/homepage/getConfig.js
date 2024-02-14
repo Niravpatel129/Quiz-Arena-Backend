02da96932822a564e3ef2935be54428269cca6c1
Nirav <niravpatelp129@gmail.com>
2024-02-13 20:26:37 -0500

    Update queueTime logic in getConfig.js

--- controllers/homepage/getConfig.js
+++ controllers/homepage/getConfig.js
@@ -7,7 +7,7 @@ const getConfig = async (req, res) => {
     let queueTime = defaultQueueTime;
 
     if (version) {
-      queueTime = version === '51' ? 9999 : defaultQueueTime;
+      queueTime = version === '52' ? 9999 : defaultQueueTime;
     }
 
     if (parseInt(version) < 24) {
