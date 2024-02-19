a2da0863eb2e2bfbedcf7b64af5e097700072486
Nirav <niravpatelp129@gmail.com>
2024-02-18 20:14:27 -0500

    Update queueTime based on version

--- controllers/homepage/getConfig.js
+++ controllers/homepage/getConfig.js
@@ -12,7 +12,7 @@ const getConfig = async (req, res) => {
     let queueTime = defaultQueueTime;
 
     if (version) {
-      queueTime = version === '52' ? 9999 : defaultQueueTime;
+      queueTime = version === '53' ? 9999 : defaultQueueTime;
     }
 
     if (parseInt(version) < 24) {
