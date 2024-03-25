fbd085d1275594eb91d40e285012374d43035001
Nirav <niravpatelp129@gmail.com>
2024-03-25 18:12:01 -0400

    Update Cloudinary API key in image upload URL

--- scripts/convertQuestionImageTypes.js
+++ scripts/convertQuestionImageTypes.js
@@ -58,7 +58,7 @@ const convertQuestionImageTypes = async () => {
         formData.append('upload_preset', 'gamercoach');
 
         const uploadResponse = await axios.post(
-          `https://api.cloudinary.com/v1_1/gamercoach/image/upload?api_key=997981818793491`,
+          `https://api.cloudinary.com/v1_1/gamercoach/image/upload?api_key=${process.env.CLOUDINARY_API_KEY}`,
           formData,
           {
             timeout: 5000,
