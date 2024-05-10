67847febb597dbfa2fa6ae467938c794025bbaf5
Nirav <niravpatelp129@gmail.com>
2024-05-10 14:11:52 -0400

    * feat(convertImages.js): updated image conversion logic

--- scripts/questionsImage/convertImages.js
+++ scripts/questionsImage/convertImages.js
@@ -18,9 +18,10 @@ const Question = require('../../models/Question');
 
 const convertImages = async () => {
   const questions = await Question.find({
-    question:
-      'Guess the prehistoric animal: A large, carnivorous marine reptile with a streamlined body and four flippers, lived during the Jurassic period.',
-    // helperImage: { $nin: [null, /cloudinary/], $exists: true, $ne: '' },
+    // category: 'sports logos',
+    // question:
+    //   'Guess the prehistoric animal: A large, carnivorous marine reptile with a streamlined body and four flippers, lived during the Jurassic period.',
+    helperImage: { $nin: [null, /cloudinary/], $exists: true, $ne: '' },
   });
 
   for (const question of questions) {
@@ -33,23 +34,34 @@ const convertImages = async () => {
       const buffer = await response.buffer();
 
       // Generate a unique filename for the image
-      const filename = `images/${question._id}.jpg`;
+      const timestamp = Date.now();
+      const filename = `images/${question._id}-${timestamp}.jpg`;
 
       // Upload the image to Google Cloud Storage
       const bucket = storage.bucket('quiz-arena-e2415.appspot.com');
-      const file = bucket.file(filename);
-      await file.save(buffer, {
+      const blob = bucket.file(filename);
+      const blobStream = blob.createWriteStream({
         metadata: {
           contentType: 'image/jpeg',
         },
       });
 
-      console.log('Image uploaded successfully:', filename);
+      await new Promise((resolve, reject) => {
+        blobStream.on('error', (err) => {
+          console.error(err);
+          reject(err);
+        });
+        blobStream.on('finish', async () => {
+          await blob.makePublic();
+          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+          console.log('Image uploaded:', publicUrl);
+          resolve(publicUrl);
+        });
+        blobStream.end(buffer);
+      });
 
       // Update the question's helperImage field with the Google Cloud Storage URL
-      const gcsUrl = `https://storage.googleapis.com/${bucket.name}/${filename}`;
-
-      question.helperImage = gcsUrl;
+      question.helperImage = blob.publicUrl();
       await question.save();
 
       console.log('Question updated with Google Cloud Storage URL:', question._id);
