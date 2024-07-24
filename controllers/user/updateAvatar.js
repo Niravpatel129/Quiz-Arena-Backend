356051c4e058cbd8bf66083c9c3e98eee617c47b
Nirav <niravpatelp129@gmail.com>
2024-07-24 02:31:52 -0400

    21

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -1,5 +1,26 @@
 const User = require('../../models/User');
 
+const updateAllAvatars = async () => {
+  try {
+    const defaultAvatar =
+      'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-jumping-in-air-wout-bg.png?alt=media&token=82811efc-ba1e-4bed-b37f-a3cc09bc1dbb';
+
+    const result = await User.updateMany(
+      { profile: { $exists: true, $ne: null } },
+      { $set: { 'profile.avatar': defaultAvatar } },
+      { upsert: false },
+    );
+
+    console.log(`Updated ${result.modifiedCount} users' avatars.`);
+    return result.modifiedCount;
+  } catch (error) {
+    console.error('Error updating all avatars:', error);
+    throw error;
+  }
+};
+
+// updateAllAvatars();
+
 const updateAvatar = async (req, res) => {
   try {
     const userId = req.userId;
@@ -22,7 +43,7 @@ const updateAvatar = async (req, res) => {
     res.status(200).json({
       message: 'Profile updated successfully',
       avatar: user.profile.avatar,
-      username: user.profile.username,
+      username: user.username,
     });
   } catch (error) {
     console.error('Error updating profile:', error);
