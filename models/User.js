6b3540bf26383e80d57622954d20aa0b361dfa8d
Nirav <niravpatelp129@gmail.com>
2024-07-24 02:26:16 -0400

    * refactor(models/User.js): updated default avatar URL

--- models/User.js
+++ models/User.js
@@ -59,7 +59,8 @@ const userSchema = new Schema(
     profile: {
       avatar: {
         type: String,
-        default: 'https://i.redd.it/zwfggeplutf61.jpg',
+        default:
+          'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-jumping-in-air-wout-bg.png?alt=media&token=82811efc-ba1e-4bed-b37f-a3cc09bc1dbb',
       },
       country: {
         type: String,
