e4564f88c8b67977d661a929b05a5897de5ade3b
Nirav <niravpatelp129@gmail.com>
2024-07-24 12:59:32 -0400

    21

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -1,25 +1,39 @@
 const User = require('../../models/User');
 
 const updateAllAvatars = async () => {
+  const randomAvatars = [
+    'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-floating-wout-bg.png?alt=media&token=15741842-42aa-4277-9e09-952c0b6483ee',
+    'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-thinking-wout-bg.png?alt=media&token=6485312e-2ae8-4fa7-8969-841e51854bca',
+    'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-angry-wout-bg.png?alt=media&token=160ffcfe-e558-4137-9358-09b98a6b1e0f',
+    'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-laying-wout-bg.png?alt=media&token=5d450f0b-426e-483e-827c-cfdf4a0c9039',
+  ];
+
   try {
-    const defaultAvatar =
-      'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-jumping-in-air-wout-bg.png?alt=media&token=82811efc-ba1e-4bed-b37f-a3cc09bc1dbb';
+    const users = await User.find({});
+    let updatedCount = 0;
 
-    const result = await User.updateMany(
-      { profile: { $exists: true, $ne: null } },
-      { $set: { 'profile.avatar': defaultAvatar } },
-      { upsert: false },
-    );
+    for (const user of users) {
+      const randomAvatar = randomAvatars[Math.floor(Math.random() * randomAvatars.length)];
 
-    console.log(`Updated ${result.modifiedCount} users' avatars.`);
-    return result.modifiedCount;
+      if (!user.profile) {
+        user.profile = { avatar: randomAvatar };
+      } else {
+        user.profile.avatar = randomAvatar;
+      }
+
+      await user.save();
+      updatedCount++;
+    }
+
+    console.log(`Updated ${updatedCount} users' avatars.`);
+    return updatedCount;
   } catch (error) {
     console.error('Error updating all avatars:', error);
     throw error;
   }
 };
 
-// updateAllAvatars();
+updateAllAvatars();
 
 const updateAvatar = async (req, res) => {
   try {
