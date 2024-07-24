826051e287d06d2fc87a3c4fb2bb31740a350071
Nirav <niravpatelp129@gmail.com>
2024-07-24 02:42:05 -0400

    21

--- controllers/leaderboards/getRankingsForCategory.js
+++ controllers/leaderboards/getRankingsForCategory.js
@@ -4,9 +4,10 @@ const getLeaderboardByCategory = async (req, res) => {
   try {
     // Decode URI component to get the category
     const category = decodeURI(req.params.category);
+    console.log('🚀  category:', category);
 
     // Use aggregation framework
-    const users = await User.aggregate([
+    let users = await User.aggregate([
       // Project fields along with a dynamic field for rating
       {
         $project: {
@@ -22,17 +23,34 @@ const getLeaderboardByCategory = async (req, res) => {
       { $limit: 10 },
     ]);
 
+    // If no users found or less than 10, get random users
+    if (users.length < 10) {
+      const randomUsers = await User.aggregate([
+        { $sample: { size: 10 - users.length } },
+        {
+          $project: {
+            username: 1,
+            'profile.avatar': 1,
+            'profile.country': 1,
+            rating: `$elo.rating.${category}`,
+          },
+        },
+      ]);
+      users = users.concat(randomUsers);
+    }
+
     // Transform the aggregation result to the desired format
     const leaderboard = users.map((user) => ({
       userId: user._id,
       username: user.username,
-      rating: user.rating,
+      rating: user.rating || 0, // Default to 0 if rating is undefined
       avatar: user.profile.avatar,
       country: user.profile.country,
     }));
 
     res.json(leaderboard);
   } catch (error) {
+    console.error('Error retrieving leaderboard:', error);
     res.status(500).send({ message: 'Error retrieving leaderboard' });
   }
 };
