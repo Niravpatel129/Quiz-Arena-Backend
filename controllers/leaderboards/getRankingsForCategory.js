0a636614ba614b4e94c4568f5da28cd6cbf089b4
Nirav <niravpatelp129@gmail.com>
2024-07-24 02:44:11 -0400

    21

--- controllers/leaderboards/getRankingsForCategory.js
+++ controllers/leaderboards/getRankingsForCategory.js
@@ -44,8 +44,10 @@ const getLeaderboardByCategory = async (req, res) => {
       userId: user._id,
       username: user.username,
       rating: user.rating || 0, // Default to 0 if rating is undefined
-      avatar: user.profile.avatar,
-      country: user.profile.country,
+      avatar:
+        user?.profile?.avatar ||
+        'https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Ffree-axolotl-jumping-in-air-wout-bg.png?alt=media&token=82811efc-ba1e-4bed-b37f-a3cc09bc1dbb',
+      country: user?.profile?.country || 'AQ',
     }));
 
     res.json(leaderboard);
